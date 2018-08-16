9dd69782f93cb85ce2ebb0e2c27beb9400b943c7
Scott Davis <scottd@collegenet.com>
2018-08-16 16:09:30 -0700

    basic commands hooked.

--- server/command_hooks.go
+++ server/command_hooks.go
@@ -10,6 +10,12 @@ import (
 
 const CommandTrigger = "remind"
 
+func runSchedule() {
+   for i := 3; i < 1000; i+=3 {
+        fmt.Printf("i loop: %v", i)
+    }
+}
+
 func (p *Plugin) registerCommand(teamId string) error {
 	if err := p.API.RegisterCommand(&model.Command{
 		TeamId:           teamId,
@@ -17,7 +23,7 @@ func (p *Plugin) registerCommand(teamId string) error {
 		AutoComplete:     true,
 		AutoCompleteHint: "[@someone or ~channel] [what] [when]",
 		AutoCompleteDesc: "Enables or disables the demo plugin hooks.",
-		DisplayName:      "Demo Plugin Command",
+		DisplayName:      "Remind Plugin Command",
 		Description:      "Set a reminder",
 	}); err != nil {
 		p.API.LogError(
@@ -26,71 +32,82 @@ func (p *Plugin) registerCommand(teamId string) error {
 		)
 	}
 
+	// runSchedule()
+
 	return nil
 }
 
 func (p *Plugin) emitStatusChange() {
-	p.API.PublishWebSocketEvent("status_change", map[string]interface{}{
-		"enabled": true,
-	}, &model.WebsocketBroadcast{})
+	// p.API.PublishWebSocketEvent("status_change", map[string]interface{}{
+	// 	"enabled": true,
+	// }, &model.WebsocketBroadcast{})
 }
 
-// ExecuteCommand executes a command that has been previously registered via the RegisterCommand
-// API.
-//
-// This demo implementation responds to a /demo_plugin command, allowing the user to enable
-// or disable the demo plugin's hooks functionality (but leave the command and webapp enabled).
 func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
-	if !strings.HasPrefix(args.Command, "/"+CommandTrigger) {
-		return &model.CommandResponse{
-			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text:         fmt.Sprintf("Unknown command: " + args.Command),
-		}, nil
-	}
 
-	if strings.HasSuffix(args.Command, "me foo in 2 seconds") {
-		return &model.CommandResponse{
-			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: "Ok!  Will remind you foo in 2 seconds",
-		}, nil
-	}
-
+	user, err := p.API.GetUser(args.UserId)
 	
-	// if strings.HasSuffix(args.Command, "true") {
-	// 	if !p.disabled {
-	// 		return &model.CommandResponse{
-	// 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-	// 			Text:         "The demo plugin hooks are already enabled.",
-	// 		}, nil
-	// 	}
+	if err != nil {
+		p.API.LogError("failed to query user %s", args.UserId)
+	}
 
-	// 	p.disabled = false
-	// 	p.emitStatusChange()
+	if strings.HasSuffix(args.Command, "help") {
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+		}, nil
+	}
 
-	// 	return &model.CommandResponse{
-	// 		ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-	// 		Text:         "Enabled demo plugin hooks.",
-	// 	}, nil
+	if strings.HasSuffix(args.Command, "list") {
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+		}, nil
+	}
 
-	// } else if strings.HasSuffix(args.Command, "false") {
-	// 	if p.disabled {
-	// 		return &model.CommandResponse{
-	// 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-	// 			Text:         "The demo plugin hooks are already disabled.",
-	// 		}, nil
-	// 	}
+	if strings.HasSuffix(args.Command, "version") {
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+		}, nil
+	}
 
-	// 	p.disabled = true
-		p.emitStatusChange()
+	payload := strings.Trim(strings.Replace(args.Command, "/"+CommandTrigger, "", -1),"")
+	commandSplit := strings.Split(payload," ")
 
-	// 	return &model.CommandResponse{
-	// 		ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-	// 		Text:         "Disabled demo plugin hooks.",
-	// 	}, nil
-	// }
+	if len(commandSplit) == 0 {	
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text:         fmt.Sprintf("No Command Found"),
+		}, nil
+	}
+
+	if commandSplit[1] == "me" {
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+		}, nil
+	}
+
+	if strings.HasPrefix(commandSplit[1][:1], "@"){
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+		}, nil
+	}
+
+	if strings.HasPrefix(commandSplit[1][:1], "~") {
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+		}, nil
+	}
+
+	p.emitStatusChange()
 
 	return &model.CommandResponse{
 		ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
 		Text:         fmt.Sprintf("Unknown command action: " + args.Command),
 	}, nil
 }
+
