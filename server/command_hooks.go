dbe787942c90c0f4613f1db53e6777521a1d7b40
Scott Davis <scottd@collegenet.com>
2018-08-16 17:56:06 -0700

    cleanup to help link

--- server/command_hooks.go
+++ server/command_hooks.go
@@ -9,13 +9,25 @@ import (
 	"github.com/mattermost/mattermost-server/plugin"
 )
 
-// var echoSem chan bool
+const Version = "0.0.1"
 
 const CommandTrigger = "remind"
 
 const ExceptionText = "Sorry, I didn’t quite get that. I’m easily confused. " +
             "Perhaps try the words in a different order? This usually works: " +
             "`/remind [@someone or ~channel] [what] [when]`.\n";
+
+const HelpText = ":wave: Need some help with `/remind`?\n" +
+            "Use `/remind` to set a reminder for yourself, someone else, or for a channel. Some examples include:\n" +
+            "* `/remind me to drink water at 3pm every day`\n" +
+            "* `/remind me on June 1st to wish Linda happy birthday`\n" +
+            "* `/remind ~team-alpha to update the project status every Monday at 9am`\n" +
+            "* `/remind @jessica about the interview in 3 hours`\n" +
+            "* `/remind @peter tomorrow \"Please review the office seating plan\"`\n" +
+            "Or, use `/remind list` to see the list of all your reminders.\n" +
+            "Have a bug to report or a feature request?  [Submit your issue here](https://gitreports.com/issue/scottleedavis/mattermost-plugin-remind).";
+
+
 func runSchedule() {
 	runOnce()
 }
@@ -109,13 +121,13 @@ func (p *Plugin) emitStatusChange() {
 	// p.API.PublishWebSocketEvent("status_change", map[string]interface{}{
 	// 	"enabled": true,
 	// }, &model.WebsocketBroadcast{})
+	fmt.Println("hahahahahahha")
 	p.API.LogError("emitStatusChange")
-	runSchedule()
+	// runSchedule()
 }
 
 
 func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
-
 
 	p.API.LogError("ExecuteCommand")
 
@@ -128,28 +140,35 @@ func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*mo
 	if strings.HasSuffix(args.Command, "help") {
 		return &model.CommandResponse{
 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: fmt.Sprintf("Ok! %v %s %s", user.Id, user.Username, user.Timezone["automaticTimezone"]),
+			Text: fmt.Sprintf(HelpText),
 		}, nil
 	}
 
 	if strings.HasSuffix(args.Command, "list") {
 		return &model.CommandResponse{
 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+			Text: fmt.Sprintf("todo"),
 		}, nil
 	}
 
 	if strings.HasSuffix(args.Command, "version") {
 		return &model.CommandResponse{
 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+			Text: fmt.Sprintf(Version),
 		}, nil
 	}
 
 	if strings.HasSuffix(args.Command, "debug") {
 		return &model.CommandResponse{
 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: fmt.Sprintf("* %s\n * %s\n * (%s)\n * %s\n", args.Command, user.Username, user.Id,  user.Timezone["automaticTimezone"]),
+			Text: fmt.Sprintf("* %s\n * %s\n * %s\n * %s\n * %s\n * (%s)\n * %s\n", 
+				args.Command, 
+				args.TeamId,
+				args.SiteURL,
+				args.Session,
+				user.Username, 
+				user.Id,  
+				user.Timezone["automaticTimezone"]),
 		}, nil
 	}
 
@@ -169,7 +188,7 @@ func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*mo
 		p.emitStatusChange()
 		return &model.CommandResponse{
 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+			Text: fmt.Sprintf("todo"),
 		}, nil
 	}
 
@@ -178,7 +197,7 @@ func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*mo
 		p.emitStatusChange()
 		return &model.CommandResponse{
 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+			Text:  fmt.Sprintf("todo"),
 		}, nil
 	}
 
@@ -187,7 +206,7 @@ func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*mo
 		p.emitStatusChange()
 		return &model.CommandResponse{
 			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
-			Text: fmt.Sprintf("Ok! %s %s", user.Username, user.Timezone["automaticTimezone"]),
+			Text:  fmt.Sprintf("todo"),
 		}, nil
 	}
 
