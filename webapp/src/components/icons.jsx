4131df87af0e1aca4ea7a9bfb254c21dc746c34d
Scott Davis <scottd@collegenet.com>
2018-08-19 18:01:22 -0700

    removed unused

--- webapp/src/components/icons.jsx
+++ webapp/src/components/icons.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 
-export const MainMenuMobileIcon = () => (
-    <i className='icon fa fa-plug'/>
-);
-
 export const ChannelHeaderButtonIcon = () => (
     <i
         className='icon fa fa-clock-o'
         style={{fontSize: '15px', position: 'relative', top: '-1px'}}
     />
-);
-
-export const FileUploadMethodIcon = () => (
-    <i className='icon fa fa-clock-o'/>
 );
