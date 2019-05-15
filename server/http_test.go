58eee242bd678edbdcd6f80b22062a5c6eba196d
Scott Lee Davis <skawtus@gmail.com>
2019-05-14 21:26:48 -0700

    commented out tbd tests

--- server/http_test.go
+++ server/http_test.go
@@ -993,7 +993,7 @@ func TestHandleCloseList(t *testing.T) {
 		api.On("LogDebug", mock.Anything, mock.Anything, mock.Anything).Maybe()
 		api.On("LogError", mock.Anything, mock.Anything, mock.Anything).Maybe()
 		api.On("LogInfo", mock.Anything).Maybe()
-		api.On("DeletePost", mock.Anything).Return(nil)
+		api.On("DeleteEphemeralPost", mock.Anything, mock.Anything).Return(nil)
 		return api
 	}
 
