53279d4bbbda580a515b10ee2020f165b7a4c96b
Dave Gittins <david.gittins@imc.com>
2019-05-27 17:23:38 +1000

    #109 Use time.Now().Location() as-is

--- server/utils.go
+++ server/utils.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/nicksnyder/go-i18n/i18n"
-	"github.com/tkuchiki/go-timezone"
 )
 
 func (p *Plugin) translation(user *model.User) (i18n.TranslateFunc, string) {
@@ -22,21 +21,8 @@ func (p *Plugin) translation(user *model.User) (i18n.TranslateFunc, string) {
 func (p *Plugin) location(user *model.User) *time.Location {
 	tz := user.GetPreferredTimezone()
 	if tz == "" {
-		tzCode, _ := time.Now().Zone()
-
-		if tzLoc, err := timezone.GetTimezones(tzCode); err != nil {
-			return time.Now().Location()
-		} else {
-			if len(tzLoc) > 0 {
-				if l, lErr := time.LoadLocation(tzLoc[0]); lErr != nil {
-					return time.Now().Location()
-				} else {
-					return l
-				}
-			} else {
-				return time.Now().Location()
-			}
-		}
+		// Use server's timezone
+		return time.Now().Location()
 	} else {
 		location, _ := time.LoadLocation(tz)
 		return location
