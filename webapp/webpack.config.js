a7329c6265f8d4ec38132b9b42f807e68a7e5d48
Scott Lee Davis <skawtus@gmail.com>
2019-07-31 16:33:13 -0700

    updated package.json dependencies and webpack config

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -20,10 +20,26 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'react'],
                         plugins: [
-                            'transform-class-properties',
-                            'transform-object-rest-spread',
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-syntax-dynamic-import',
+                        ],
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    chrome: 66,
+                                    firefox: 60,
+                                    edge: 42,
+                                    safari: 12,
+                                },
+                                modules: false,
+                                debug: false,
+                                useBuiltIns: 'usage',
+                                shippedProposals: true,
+                            }],
+                            ['@babel/preset-react', {
+                                useBuiltIns: true,
+                            }],
                         ],
                     },
                 },
@@ -34,6 +50,8 @@ module.exports = {
         react: 'React',
         redux: 'Redux',
         'react-redux': 'ReactRedux',
+        'prop-types': 'PropTypes',
+        'react-bootstrap': 'ReactBootstrap',
     },
     output: {
         path: path.join(__dirname, '/dist'),
